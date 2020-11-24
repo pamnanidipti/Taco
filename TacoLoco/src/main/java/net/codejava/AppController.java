9275e6cd910d99998c2490b3e38d100ae0531228
Dipti <pamnanidipti@gmail.com>
2020-11-24 00:33:45 -0600

    Added sql script and code comments

--- TacoLoco/src/main/java/net/codejava/AppController.java
+++ TacoLoco/src/main/java/net/codejava/AppController.java
@@ -16,7 +16,10 @@ public class AppController {
 
 	@Autowired
 	private CustomerDAO dao;
-	
+	/**
+	* This method renders the home page
+	* 
+	*/
 	@RequestMapping("/")
 	public String viewHomePage(Model model) {
 		List<Customer> listCustomer = dao.list();
@@ -24,6 +27,10 @@ public class AppController {
 		return "index";
 	}
 	
+	/**
+	* This method opens a new form
+	* to help create new customer
+	*/
 	@RequestMapping("/new")
 	public String showNewForm(Model model) {
 		Customer customer = new Customer();
@@ -31,12 +38,20 @@ public class AppController {
 		return "new_form";
 	}
 	
+	/**
+	* This method initiates the save request, and redirects
+	* to home page
+	*/
 	@RequestMapping(value="/save", method = RequestMethod.POST)
 	public String save(@ModelAttribute("customer") Customer customer) {
 		dao.save(customer);
 		return "redirect:/";
 	}
 	
+	/**
+	* This method renders the edit form, with the customer details
+	* populated
+	*/
 	@RequestMapping("/edit/{id}")
 	public ModelAndView showEditForm(@PathVariable(name = "id") int id) {
 		ModelAndView mav = new ModelAndView("edit_form");
@@ -45,12 +60,19 @@ public class AppController {
 		return mav;
 	}
 	
+	/**
+	* This method initiates the update request, and redirects
+	* to home page
+	*/
 	@RequestMapping(value="/update", method = RequestMethod.POST)
 	public String update(@ModelAttribute("customer") Customer customer) {
 		dao.update(customer);
 		return "redirect:/";
 	}
 	
+	/**
+	* This method initiates the delete record request
+	*/
 	@RequestMapping("/delete/{id}")
 	public String delete(@PathVariable(name = "id") int id) {
 		dao.delete(id);
